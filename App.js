41b2bfa1094198bbd8a93a72651a9c91f1cebda2
monishasakthivel <93466227+monishasakthivel@users.noreply.github.com>
2021-10-31 13:26:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello friend!</h1>
+      <h2>Awaiting loger to see you !</h2>
+      <p>Hope We'll meet in couple of days :)</p>
+      <h3>waiting to see you buddy..!!</h3>
+     </div>
+  );
+}
